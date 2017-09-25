177a18a2950d2d1ec061be6231491691758423ba
Jérémy Dillenbourg <jeremy.dillenbourg@famousgrey.com>
2017-09-25 16:48:35 +0200

    init

---
+++ Communicator.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Libraries\Famous\HttpCommunicator;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7;
+
+/**
+ * Implement Guzzle stuff to send request to API
+ * Class Communicator
+ * @package App\Libraries\Famous\HttpCommunicator
+ */
+class Communicator
+{
+    protected $_client = null;
+    protected $_response = null;
+    protected $_path = null;
+    protected $_data = [];
+    protected $_json = false;
+    protected $_files = false;
+    protected $_base_uri = 'api';
+
+    public function __construct($baseUri='', array $headers)
+    {
+        $this->_base_uri = $baseUri;
+        $this->buildClient( $headers );
+        return $this;
+    }
+
+    protected function buildClient(array $headers = []) {
+        $this->_client = new Client([
+            'base_uri'  =>  $this->_base_uri,
+            'headers'      => $headers
+        ]);
+        return $this;
+    }
+
+    public function get( $path, array $data) {
+        $this->_path = $path;
+        $this->_data = $data;
+        return $this->call('GET');
+    }
+
+    /**
+     * @param string $path (relative path only, not host part)
+     * @param array $data ['key' => 'value']
+     * @return bool|\Psr\Http\Message\ResponseInterface
+     */
+    public function post( $path, array $data) {
+        $this->_path = $path;
+        $this->_data = $data;
+        return $this->call('POST');
+    }
+    public function put( $path, array $data) {
+        $this->_path = $path;
+        $this->_data = $data;
+        return $this->call('PUT');
+    }
+    public function delete( $path, array $data) {
+        $this->_path = $path;
+        $this->_data = $data;
+        return $this->call('DELETE');
+    }
+
+    /**
+     * @param bool $value
+     * @return $this
+     */
+    public function useJson( $value = true) {
+        $this->_json = $value;
+        return $this;
+    }
+
+    /**
+     * @param bool $value
+     * @return $this
+     */
+    public function useFiles( $value = true) {
+        $this->_files = $value;
+        return $this;
+    }
+
+    protected function call( $method) {
+
+        $requestData = $this->getRequestData($method);
+
+        try {
+            $this->_response = $this->_client->request($method, $this->_path, $requestData);
+
+        } catch (ClientException $e) {
+            if(env('APP_DEBUG')) {
+                echo Psr7\str($e->getRequest());
+                echo Psr7\str($e->getResponse());
+            } else {
+                \Log::error(Psr7\str($e->getRequest()));
+                \Log::error(Psr7\str($e->getResponse()));
+            }
+            die;
+        } catch (RequestException $e) {
+            if(env('APP_DEBUG')) {
+                echo Psr7\str($e->getRequest());
+                if ($e->hasResponse()) {
+                    echo Psr7\str($e->getResponse());
+                }
+            } else {
+                \Log::error(Psr7\str($e->getRequest()));
+                if ($e->hasResponse()) {
+                    \Log::error(Psr7\str($e->getResponse()));
+                }
+            }
+            die;
+        }
+
+        $response = $this->_response;
+        $this->reset();
+        return $response;
+    }
+
+    protected function getRequestData( $method) {
+
+        if($method == 'GET') {
+            $term = 'query';
+            $data = $this->_data;
+        } else {
+            if($this->_json) {
+                $term = 'json';
+                $data = empty($this->_data) ? null : $this->_data;
+            } elseif($this->_files) {
+                $term = 'multipart';
+
+                $data = [];
+
+                foreach($this->_data as $key=>$value) {
+                    $data[] = [
+                        'name'      => $key,
+                        'content'   => $value
+                    ];
+                }
+            } else {
+                $term = 'form_params';
+                $data = $this->_data;
+            }
+        }
+
+        return [
+            $term => $data
+        ];
+    }
+
+    protected function reset() {
+        $this->_response = null;
+        $this->_path = null;
+        $this->_data = [];
+        $this->_json = false;
+        $this->_files = false;
+    }
+}
